ba1acfd3733f19acf8267d842a6e2d75689f2cbf
Juan Esteban <valdezjuanes014@gmail.com>
2024-10-10 11:21:06 -0500

    inicio repo

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/prueba")
+async def ping():
+    return {"message": "prueba OK"}
